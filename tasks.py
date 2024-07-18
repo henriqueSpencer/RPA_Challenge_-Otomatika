3018cf3a134cf41541f4a6a85002185dab214e2f
henriqueSpencer <henriquespencer11@gmail.com>
2024-07-18 00:46:32 -0300

    Handle parameters

--- tasks.py
+++ tasks.py
@@ -186,17 +186,32 @@ class ExcelHandler:
             self.lib.save_workbook()
             self.lib.close_workbook()
 
+
 @task
 def task_handler():
-    item = workitems.inputs.current
-    print("Received payload:", item.payload)
-    logger.info("Received payload:", item.payload)
+    """ This task will get the news from the AP News site and save it to an excel file"""
+    info = handle_parametres()
     ap = ApNewsSite()
-    news = ap.get_news("Covid", category="Health", months_to_download=10)
+    news = ap.get_news(info['news'], category=info['category'], months_to_download=info['months_to_download'])
     excel = ExcelHandler()
     excel.save_news_to_excel(news)
     logger.info('Finished')
 
+
+def handle_parametres():
+    """This method will handle the parameters received from the input of the task"""
+    return_info = {}
+    item = workitems.inputs.current
+    print("Received payload:", item.payload)
+    logger.info(f"Received payload: {item.payload}")
+    dict_parametres = item.payload
+    return_info['news'] = dict_parametres.get['news'] if dict_parametres.get['news'] is not None else 'Covid'
+    return_info['category'] = dict_parametres.get['category'] if dict_parametres.get[
+                                                                     'category'] is not None else "Health"
+    return_info['months_to_download'] = dict_parametres.get['months_to_download'] if dict_parametres.get[
+                                                                                         'months_to_download'] is not None else 2
+
+    return return_info
 
 # if __name__ == '__main__':
 #     task_handler()
